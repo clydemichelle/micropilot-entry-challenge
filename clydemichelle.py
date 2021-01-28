7646e5498c04f96f33a496982658f7d97ca05753
clydemichelle <clydemichelle022@gmail.com>
2021-01-28 11:57:12 +0300

    Updated py solution

--- clydemichelle.py
+++ clydemichelle.py
@@ -9,15 +9,21 @@ Created on Thu Jan 28 09:37:48 2021
 x1 = -2
 x2 = 5
 
-#Function to solve greater than comparison
-def getX(x1,x2):
-    if x1 > x2:
-        print("x1:",x1," is greater than x2",x2)
-    elif x2 > x1:
-        print("x2:",x2, "is greater than x1:",x1)
-    else:
-        print("Both values are equal")
- 
-#Calling the function
-greaterX = getX(x1, x2)
-print(greaterX)
+#Adding the variable to a list
+X = []
+X.append(x1)
+X.append(x2)
+
+
+#function to get greatest value of X
+def getX(X):
+#Traversing through the list
+    for i in X:
+        greaterX = 0
+        if i > greaterX:
+            greaterX+=i
+            print(i)
+
+greatestX = getX(X)
+print(greatestX)
+        
